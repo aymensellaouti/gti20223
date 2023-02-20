63ddb9c6e67bc2bfd47ed83dbe4f14ee1069396a
aymen2782 <aymen.sellaouti@gmail.com>
2023-02-20 11:48:20 +0100

    repeatDirective custom strutral directive

--- src/app/directives/repeat.directive.ts
+++ src/app/directives/repeat.directive.ts
@@ -7,6 +7,22 @@ import {
   ViewContainerRef,
 } from '@angular/core';
 
+class RepeatContext {
+  constructor(
+    public index = 0,
+    public totalIteration = 0,
+    public paire = false,
+    public impaire = false,
+    public first = false,
+    public last = false
+  ) {
+    this.paire = !!(this.index % 2);
+    this.impaire = !this.paire;
+    this.first = !this.index;
+    this.last = this.index == this.totalIteration - 1;
+  }
+}
+
 @Directive({
   selector: '[appRepeat]',
 })
@@ -19,7 +35,8 @@ export class RepeatDirective implements OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     this.vcr.clear();
     for (let i = 0; i < this.nbIteration; i++) {
-      this.vcr.createEmbeddedView(this.template);
+      let context = new RepeatContext(i, this.nbIteration);
+      this.vcr.createEmbeddedView(this.template, context);
     }
   }
 }
