673db255d11316d103e8b973b7d8d8c66744f05c
aymen2782 <aymen.sellaouti@gmail.com>
2023-02-24 11:32:29 +0100

    test Find All Cvs

--- src/app/cv/services/cv.service.spec.ts
+++ src/app/cv/services/cv.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { CvService } from './cv.service';
+import { Cv } from '../model/cv';
+import { API } from '../../../config/api.config';
 
-describe('CvService', () => {
+fdescribe('CvService', () => {
   let service: CvService;
-
+  let http: HttpTestingController;
+  let cvs: Cv[] = [];
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CvService);
+    http = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should return all cvs', () => {
+    /* 1  Appelez la méthode testée qui envoie des requêtes HTTP. */
+    service.getCvs().subscribe((listCvs) => (cvs = listCvs));
+    /* 2 Récupérer les requêtes en attente  */
+    const req = http.expectOne(API.cv);
+    /* 3 Répondre à ces requêtes avec de fausses données. */
+    req.flush(service.getFakeCvs());
+    /* 4 Vérifier le résultat de l'appel de méthode */
+    expect(cvs).toBeTruthy();
+    expect(cvs.length).toBe(2);
+    expect(cvs[0].id).toBe(1);    /* 5 Vérifier que toutes les requêtes ont été générées */
+  });
+  it('should return cv By Id', () => {
+    /* 1  Appelez la méthode testée qui envoie des requêtes HTTP. */
+    /* 2 Récupérer les requêtes en attente  */
+    /* 3 Répondre à ces requêtes avec de fausses données. */
+    /* 4 Vérifier le résultat de l'appel de méthode */
+    /* 5 Vérifier que toutes les requêtes ont été générées */
   });
 });
