b3518c29216d355ec1f788301af16fda163e75ae
aymen2782 <aymen.sellaouti@gmail.com>
2023-02-24 10:07:00 +0100

    test MathService

--- src/app/services/math.service.spec.ts
+++ src/app/services/math.service.spec.ts
@@ -8,4 +8,11 @@ fdescribe('MathService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should Add two numbers', () => {
+    expect(service.add(2, 3)).toBe(5);
+  });
+  it('should Substract two numbers', () => {
+    expect(service.substract(2, 3)).toBe(-1);
+  });
 });
--- src/app/services/math.service.ts
+++ src/app/services/math.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MathService {
-
-  constructor() { }
+  constructor() {}
+  add(x: number, y: number): number {
+    /*     this.logger.logger(`${x} + ${y} = ${x + y}`); */
+    return x + y;
+  }
+  substract(x: number, y: number): number {
+    /*     this.logger.logger(`${x} - ${y} = ${x - y} `);
+     */ return x - y;
+  }
 }
