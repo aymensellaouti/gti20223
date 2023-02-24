57650cf8583cd3a131fae70c7bdd51e5e7fdfb05
aymen2782 <aymen.sellaouti@gmail.com>
2023-02-24 11:47:30 +0100

    HttpService test

--- src/app/cv/services/cv.service.spec.ts
+++ src/app/cv/services/cv.service.spec.ts
@@ -7,11 +7,17 @@ import {
 import { CvService } from './cv.service';
 import { Cv } from '../model/cv';
 import { API } from '../../../config/api.config';
+import { HttpErrorResponse } from '@angular/common/http';
 
 fdescribe('CvService', () => {
   let service: CvService;
   let http: HttpTestingController;
   let cvs: Cv[] = [];
+  let cv: Cv;
+  let error: HttpErrorResponse;
+  const status = 404;
+  const statusText = 'Cv not found';
+  const progressEvent = new ProgressEvent('Cv Not Found');
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -33,13 +39,54 @@ fdescribe('CvService', () => {
     /* 4 Vérifier le résultat de l'appel de méthode */
     expect(cvs).toBeTruthy();
     expect(cvs.length).toBe(2);
-    expect(cvs[0].id).toBe(1);    /* 5 Vérifier que toutes les requêtes ont été générées */
+    expect(cvs[0].id).toBe(
+      1
+    ); /* 5 Vérifier que toutes les requêtes ont été générées */
   });
-  it('should return cv By Id', () => {
-    /* 1  Appelez la méthode testée qui envoie des requêtes HTTP. */
-    /* 2 Récupérer les requêtes en attente  */
-    /* 3 Répondre à ces requêtes avec de fausses données. */
-    /* 4 Vérifier le résultat de l'appel de méthode */
-    /* 5 Vérifier que toutes les requêtes ont été générées */
+  it('shuould find cv by id', () => {
+    service.getCvById(1).subscribe({
+      next: (cvResponse: Cv) => {
+        cv = cvResponse;
+      },
+      /* complete: () => fail(), */
+      error: (e: HttpErrorResponse) => fail(),
+    });
+    const req = http.expectOne(API.cv + 1);
+    req.flush(cvs.find((actualCv) => actualCv.id == 1) ?? null);
+    expect(cv).toBeTruthy();
+    expect(cv.id).toBe(1);
+    expect(cv.name).toBe('sellaouti');
+    expect(req.request.method).toBe('GET');
   });
+  it('shuould return 404 when cv dosnt exist', () => {
+    service.getCvById(3).subscribe({
+      next: () => fail(),
+      complete: () => fail(),
+      error: (e: HttpErrorResponse) => {
+        error = e;
+      },
+    });
+    const req = http.expectOne(API.cv + 3);
+    expect(req.request.method).toBe('GET');
+    req.error(progressEvent, { status, statusText });
+    expect(error.status).toBe(status);
+    expect(error.statusText).toBe(statusText);
+  });
+  it('shuould delete cv by id', () => {
+    let deleteResponse: { count: number } = { count: 0 };
+    service.deleteCvById(1).subscribe((response) => {
+      deleteResponse = response;
+    });
+    const req = http.expectOne(API.cv + 1);
+
+    expect(req.request.method).toBe('DELETE');
+
+    req.flush({
+      count: cvs.length - cvs.filter((cv) => cv.id == 1).length,
+    });
+    expect(deleteResponse?.count).toBe(1);
+  });
+  // todo : A valider la bonne utilisation
+  /* 5 Vérifier que toutes les requêtes ont été gérées */
+  afterEach(() => http.verify());
 });
