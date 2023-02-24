a8c4b492dfc1f34f798253689440991320f2a129
aymen2782 <aymen.sellaouti@gmail.com>
2023-02-24 11:11:54 +0100

    test with spyOn

--- src/app/services/math.service.spec.ts
+++ src/app/services/math.service.spec.ts
@@ -1,14 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MathService } from './math.service';
+import { LoggerService } from './logger.service';
 
-let loggerSpy;
+let loggerSpy: jasmine.Spy;
 let mathService: MathService;
+let loggerService: LoggerService;
 
 fdescribe('MathService', () => {
   beforeEach(() => {
-    loggerSpy = jasmine.createSpyObj('LoggerService', ['logger']);
-    mathService = new MathService(loggerSpy);
+    /* loggerSpy = jasmine.createSpyObj('LoggerService', ['logger']); */
+    loggerService = new LoggerService();
+    loggerSpy = spyOn(loggerService, 'logger');
+    mathService = new MathService(loggerService);
   });
   it('should be created', () => {
     expect(mathService).toBeTruthy();
@@ -16,6 +20,8 @@ fdescribe('MathService', () => {
 
   it('should Add two numbers', () => {
     expect(mathService.add(2, 3)).toBe(5);
+    expect(loggerSpy.calls.count()).toBe(1);
+    expect(loggerSpy.calls.argsFor(0)).toEqual(['2 + 3 = 5']);
   });
   it('should Substract two numbers', () => {
     expect(mathService.substract(2, 3)).toBe(-1);
